8d65031107bcf738938bd777928983d7b1893a23
Jonathan Andrews <ionathas78@hotmail.com>
2020-04-14 18:07:23 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,98 @@
+var fs = require("fs");
+var express = require("express");
+
+var app = express();
+
+
+var PORT = 9733;
+
+
+// * The application frontend has already been created, it's your job to build the backend and connect the two.
+
+
+// Routes
+// ===========================================================
+
+// * The following HTML routes should be created:
+
+//   * GET `/notes` - Should return the `notes.html` file.
+
+app.get("/notes", function(req, res) {
+    renderNotesPage(req, res);
+});
+  
+//   * GET `*` - Should return the `index.html` file
+
+app.get("*", function(req, res) {
+    renderWelcomePage(req, res);
+});
+  
+// * The application should have a `db.json` file on the backend that will be used to store and retrieve notes using the `fs` module.
+
+// * The following API routes should be created:
+
+//   * GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
+
+app.get("/api/notes", function(req, res) {
+    renderNotesDB(req, res);
+});
+
+//   * POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
+
+app.post("/api/notes", function(req, res) {
+
+});
+
+//   * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+
+app.delete("/api/notes/:id", function(req, res) {
+
+});
+
+
+//  **  Functions
+
+function renderWelcomePage(req, res) {
+    fs.readFile(__dirname + "/public/index.html", function(err, data) {
+        if (err) {
+            res.writeHead(500, { "Content-Type": "text/html" });
+            res.end("<html><head><title>Oops</title></head><body><h1>Oops, there was an error</h1></html>");
+        } else {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(data);
+        };
+    });
+}
+    
+function renderNotesPage(req, res) {
+    fs.readFile(__dirname + "/public/notes.html", function(err, data) {
+        if (err) {
+            res.writeHead(500, { "Content-Type": "text/html" });
+            res.end("<html><head><title>Oops</title></head><body><h1>Oops, there was an error</h1></html>");
+        } else {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(data);
+        };
+    });
+};
+
+function renderNotesDB(req, res) {
+    fs.readFile(__dirname + "/db/db.json", function(err, data) {
+        if (err) {
+            res.writeHead(500, { "Content-Type": "text/html" });
+            res.end("<html><head><title>Oops</title></head><body><h1>Oops, there was an error</h1></html>");
+        } else {
+            res.writeHead(200, { "Content-Type": "text/JSON" });
+            res.end(data);
+        };
+    });
+}
+
+
+
+  // Listener
+  // ===========================================================
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
